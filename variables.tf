2bf76c84c280200e14ec896048d8f3c428a63bc8
joaogmn <Joao.Meira@tcbrands.com>
2024-06-27 15:40:29 -0700

    first commit

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "resource_group_name" {
   type        = string
   description = "(Optional) Name of resource group to create. Defaults to oidc-test."
-  default     = "oidc-simple-04062023"
+  default     = "oidc-simple-06272024"
 }
 
 variable "location" {
   type        = string
   description = "(Optional) Azure region to use. Defaults to East US."
-  default     = "eastus"
+  default     = "westus3"
 }
